67ffceea75e11be6ae595a60685dcdda91fdbe5d
Afzal-Hameeth <2000163830@hexaware.com>
2025-11-19 14:34:50 +0530

    Initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-white text-6xl font-bold text-blue-600">
+      Hello Tailwind + Vite + React!
+    </div>
+  )
+}
+
+export default App
